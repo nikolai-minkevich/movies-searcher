dcdd9f6a28e5bc5adcd6c24be4cea8ba030c0e74
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-04-12 01:15:20 +0300

    feat: add movie page

--- app/components/movie/detailed.js
+++ app/components/movie/detailed.js
@@ -1,8 +1,12 @@
 import Component from '@glimmer/component';
-
+const THEMOVIEDB_IMG = 'https://image.tmdb.org/t/p/w500';
 export default class MovieDetailedComponent extends Component {
-  get nothing() {
-    console.log(this.args.movie);
-    return null;
+  get year() {
+    const { release_date } = this.args.movie;
+    return release_date.substr(0, 4);
+  }
+  get poster() {
+    const { poster_path } = this.args.movie;
+    return THEMOVIEDB_IMG + poster_path;
   }
 }
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -3,12 +3,13 @@ import ENV from '../config/environment';
 const TMDB_API = 'https://api.themoviedb.org/3';
 
 export default class MovieRoute extends Route {
-  async model() {
-    console.log('ROUTE MOVIE', this.params.id);
+  async model(params) {
+    console.log('ROUTE MOVUE???');
     let response = await fetch(
-      `${TMDB_API}/movie/${this.params.id}?api_key=${ENV.TMDB_API_KEY}&language=en-US`
+      `${TMDB_API}/movie/${params.id}?api_key=${ENV.TMDB_API_KEY}&language=en-US`
     );
     let parsed = await response.json();
+    console.log(parsed);
     return parsed;
   }
 }
