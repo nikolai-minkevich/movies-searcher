2bb484f28eb552176519024b5b3ef90a0c64d6fb
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-04-12 06:40:02 +0300

    feat: one more step for scroll

--- app/_controllers/things.js
+++ app/_controllers/things.js
@@ -5,6 +5,9 @@ import Controller, { inject as injectController } from '@ember/controller';
 import generateThings from '../utils/generate-things';
 import { tracked } from '@glimmer/tracking';
 
+import ENV from '../config/environment';
+const TMDB_API = 'https://api.themoviedb.org/3';
+
 export default class ThingsController extends Controller {
   @injectController('application') appController;
 
@@ -14,18 +17,38 @@ export default class ThingsController extends Controller {
 
   constructor() {
     super(...arguments);
-    this.things = this._generateThings();
+    // this.things = this._generateThings();
+    this.things = [];
+    this.initThings();
+  }
+
+  async initThings() {
+    let res = await this.loadData();
+    console.log(res.results);
+    this.things = [...this.things, ...res.results];
   }
 
   @action
   handleLoadMore() {
     return new Promise((resolve) => {
-      later(() => {
+      later(async () => {
+        let res = await this.loadData();
+        console.log(res.results);
         this.page++;
-        this.things = [...this.things, ...this._generateThings()];
+        this.things = [...this.things, ...res];
         resolve();
       }, this.appController.loadDelay);
     });
+  }
+
+  async loadData() {
+    let page = 1;
+    let response = await fetch(
+      `${TMDB_API}/movie/popular?api_key=${ENV.TMDB_API_KEY}&language=en-US&page=${page}`
+    );
+    let parsed = await response.json();
+    console.log(parsed);
+    return parsed;
   }
 
   _generateThings() {
