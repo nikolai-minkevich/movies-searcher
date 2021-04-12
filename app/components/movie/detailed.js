0f911bd7a09189826b1fec254fb42a7ada078ac9
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-04-12 03:42:13 +0300

    fix: fix bug with routing

--- app/components/movie/detailed.js
+++ app/components/movie/detailed.js
@@ -1,6 +1,10 @@
 import Component from '@glimmer/component';
 const THEMOVIEDB_IMG = 'https://image.tmdb.org/t/p/w500';
 export default class MovieDetailedComponent extends Component {
+  get nothing() {
+    console.log(this.args.movie);
+    return null;
+  }
   get year() {
     const { release_date } = this.args.movie;
     return release_date.substr(0, 4);
@@ -8,5 +12,33 @@ export default class MovieDetailedComponent extends Component {
   get poster() {
     const { poster_path } = this.args.movie;
     return THEMOVIEDB_IMG + poster_path;
+  }
+  get genres() {
+    const { genres } = this.args.movie;
+    if (!genres) return null;
+    let genres_string = '';
+    genres.map((genre, index) => {
+      genres_string += genre.name;
+      if (index !== genres.length - 1) {
+        genres_string += ', ';
+      }
+    });
+    return genres_string;
+  }
+
+  get adult() {
+    const { adult } = this.args.movie;
+    if (adult) {
+      console.log('ok');
+    }
+    return adult;
+  }
+
+  get original_title() {
+    const { original_title, title } = this.args.movie;
+    if (original_title !== title) {
+      return original_title;
+    }
+    return null;
   }
 }
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -4,12 +4,12 @@ const TMDB_API = 'https://api.themoviedb.org/3';
 
 export default class MovieRoute extends Route {
   async model(params) {
-    console.log('ROUTE MOVUE???');
-    let response = await fetch(
+    console.log('ROUTE MOVUE???', params);
+    let response_movie = await fetch(
       `${TMDB_API}/movie/${params.id}?api_key=${ENV.TMDB_API_KEY}&language=en-US`
     );
-    let parsed = await response.json();
-    console.log(parsed);
-    return parsed;
+    let parsed_movie = await response_movie.json();
+    console.log(parsed_movie);
+    return parsed_movie;
   }
 }
